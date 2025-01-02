d9941b68e1c12193571f1ec35d92dd4b837258ee
Zoddo <git@zoddo.fr>
2025-01-02 18:34:53 +0100

    Add GPL-3.0-or-later license

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+/**
+ * @Vexera/log npm package
+ * Copyright (C) Zoddo <discord-bots@zoddo.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either version
+ * 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
 import graylog from 'gelf-pro';
 import * as constants from './constants';
 import * as callbacks from './callbacks';
